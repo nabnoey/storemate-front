90fca3569fd2edc0cf00452c4211b92f5875b952
Ntkfxy <natthaan57101@gmail.com>
2026-02-23 10:32:49 +0700

    update loading page

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,38 @@
-import { StrictMode } from 'react'
+// main.tsx
+import { StrictMode, useEffect } from 'react' 
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import router from './router'
-import {store} from './redux/store'
-import { Provider } from 'react-redux'
+import { store } from './redux/store'
+import { Provider, useDispatch } from 'react-redux' 
 import { RouterProvider } from 'react-router-dom'
-// import { UserContextProvider } from './context/UserContextProvider.tsx'
+import Loading from "./components/Loading";
+import { stopLoading } from './redux/loading/loadingReducer' 
 
+// --- สร้าง Component เพื่อจัดการ Logic ตอนเปิดเว็บ ---
+export default function AppInitializer({ children }: { children: React.ReactNode }) {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // ตั้งเวลาโหลดเทียมๆ 1.5 วินาที (หรือจะใส่ Logic เช็ค User API ตรงนี้ก็ได้)
+    const timer = setTimeout(() => {
+      dispatch(stopLoading());
+    }, 1500);
+
+    return () => clearTimeout(timer);
+  }, [dispatch]);
+
+  return <>{children}</>;
+};
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-<RouterProvider router={router} />
-</Provider>
-
-  
-  </StrictMode>,
+      {/* เรียกใช้ AppInitializer ด้านใน Provider */}
+      <AppInitializer>
+        <Loading />
+        <RouterProvider router={router} />
+      </AppInitializer>
+    </Provider>
+  </StrictMode>
 )
-
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth/authReducer";
+import loadingReducer from "./loading/loadingReducer"
 
 
 export const store = configureStore({
     reducer: {
-        auth: authReducer
+        auth: authReducer,
+        loading: loadingReducer
     },
 devTools: true
 
